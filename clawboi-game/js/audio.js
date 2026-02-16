8ffef81f941c0a963ca3bd146b9f38edbfdec6b8
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:21:22 -0800

    Update audio.js

--- clawboi-game/js/audio.js
+++ clawboi-game/js/audio.js
@@ -1,19 +1,1 @@
-export class AudioSys{
 
-constructor(){
-this.map={};
-}
-
-load(name,src){
-let a=new Audio(src);
-this.map[name]=a;
-}
-
-play(name){
-let a=this.map[name];
-if(!a)return;
-a.currentTime=0;
-a.play();
-}
-
-}
