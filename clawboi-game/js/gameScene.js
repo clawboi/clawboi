e420dbda351d81e7d042d484d7826258d661f386
clawboi <ethandawsonsound@gmail.com>
2026-02-16 03:15:54 -0800

    Update gameScene.js

--- clawboi-game/js/gameScene.js
+++ clawboi-game/js/gameScene.js
@@ -19,6 +19,10 @@ this.player=new Player(this.world.spawn.x,this.world.spawn.y);
 this.enemies=new EnemyManager(this.world);
 this.drops=new Drops();
 this.pickups=new Pickups();
+this.boss=new Boss(this.world.spawn.x+300,this.world.spawn.y);
+this.portal=new Portal(this.world.spawn.x-200,this.world.spawn.y);
+this.quest=new Quests();
+this.fx=new Particles();
 
 this.cam=cam;
 this.input=input;
@@ -57,6 +61,31 @@ this.waveTimer=10;
 }
 
 this.cam.setWorld(this.world.worldW,this.world.worldH);
+this.boss.update(dt,this.world,this.player);
+
+let atk=this.input.attack()?this.player.attack():null;
+if(atk){
+let dx=this.boss.x-atk.x;
+let dy=this.boss.y-atk.y;
+if(dx*dx+dy*dy<400){
+this.boss.hit(atk.dmg);
+this.fx.burst(atk.x,atk.y);
+}
+}
+
+if(this.boss.dead){
+this.portal.active=true;
+this.quest.done("boss");
+}
+
+if(this.pickups.done())
+this.quest.done("shards");
+
+if(this.portal.check(this.player))
+location.reload();
+
+this.fx.update(dt);
+
 this.cam.follow(dt,this.player.x,this.player.y);
 }
 
@@ -68,6 +97,11 @@ this.world.draw(ctx,o);
 this.pickups.draw(ctx,o);
 this.drops.draw(ctx,o);
 this.enemies.draw(ctx,o);
+this.boss.draw(ctx,o);
+this.portal.draw(ctx,o);
+this.fx.draw(ctx,o);
+drawMini(ctx,this.world,this.player);
+
 this.player.draw(ctx,o);
 
 drawHUD(ctx,this.player);
