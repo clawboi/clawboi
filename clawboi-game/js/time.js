3b6d2a13e9dbff8a7d05507e5df87002644d9c12
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:25:32 -0800

    Update time.js

--- clawboi-game/js/time.js
+++ clawboi-game/js/time.js
@@ -1,14 +1,1 @@
-import { now } from "./utils.js";
-import { CONFIG } from "./config.js";
 
-export class Clock{
-  constructor(){
-    this.t = now();
-  }
-  tick(){
-    const n = now();
-    const dt = (n - this.t)/1000;
-    this.t = n;
-    return Math.min(CONFIG.DT_CAP, Math.max(0, dt));
-  }
-}
