e16681d7fa424ca531558dadb72f64aec58a9b04
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:26:13 -0800

    Update renderer.js

--- clawboi-game/js/renderer.js
+++ clawboi-game/js/renderer.js
@@ -1,13 +1,1 @@
-export class Renderer{
-  constructor(canvas){
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d", { alpha:true });
-    this.ctx.imageSmoothingEnabled = false;
-  }
-  clear(color){
-    const c = this.ctx;
-    c.setTransform(1,0,0,1,0,0);
-    c.fillStyle = color;
-    c.fillRect(0,0,this.canvas.width,this.canvas.height);
-  }
-}
+
