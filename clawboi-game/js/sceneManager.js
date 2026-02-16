e74906157f1829b1860bde5907665327a4e68993
clawboi <ethandawsonsound@gmail.com>
2026-02-16 04:39:37 -0800

    Update sceneManager.js

--- clawboi-game/js/sceneManager.js
+++ clawboi-game/js/sceneManager.js
@@ -1,12 +1,28 @@
 export class Scenes{
-
-constructor(){this.stack=[]}
-
-push(s){this.stack.push(s)}
-pop(){this.stack.pop()}
-cur(){return this.stack[this.stack.length-1]}
-
-update(dt){let s=this.cur();if(s&&s.update)s.update(dt)}
-draw(ctx){let s=this.cur();if(s&&s.draw)s.draw(ctx)}
-
+  constructor(){
+    this.stack = [];
+  }
+  push(scene){
+    this.stack.push(scene);
+    if (scene && typeof scene.onEnter === "function") scene.onEnter();
+  }
+  pop(){
+    const s = this.stack.pop();
+    if (s && typeof s.onExit === "function") s.onExit();
+    return s;
+  }
+  clear(){
+    while(this.stack.length) this.pop();
+  }
+  top(){
+    return this.stack[this.stack.length-1] || null;
+  }
+  update(dt){
+    const s = this.top();
+    if (s && typeof s.update === "function") s.update(dt);
+  }
+  draw(ctx){
+    const s = this.top();
+    if (s && typeof s.draw === "function") s.draw(ctx);
+  }
 }
