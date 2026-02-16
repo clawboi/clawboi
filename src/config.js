f785235a2da7960c8d70bc6fa5bc8f14adfb8530
clawboi <ethandawsonsound@gmail.com>
2026-02-15 22:55:15 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,16 @@
+export const CONFIG = {
+  baseW: 320,
+  baseH: 180,
+
+  // scaling rules (canvas is base * scale)
+  minScale: 3,
+  maxScale: 7,
+
+  // fixed timestep base
+  dtCap: 1/30,
+
+  // visual
+  bg: "#050508",
+  violet: "#8a2eff",
+};
 
