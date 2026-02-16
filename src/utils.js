e4d9bcf9d848fdd64439bda6fac2bdf64c675445
clawboi <ethandawsonsound@gmail.com>
2026-02-15 23:14:55 -0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 export const clamp = (n, a, b) => Math.max(a, Math.min(b, n));
 export const lerp  = (a, b, t) => a + (b - a) * t;
+export const rand = (a,b)=> a + Math.random()*(b-a);
+export const rint = (a,b)=> (a + Math.random()*(b-a+1))|0;
 
 export function now(){
   return performance.now();
