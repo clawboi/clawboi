453bdb84d561b57f72ce04a4b281d1ab2b6296ea
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:39:04 -0800

    Update sceneManager.js

--- clawboi-game/js/sceneManager.js
+++ clawboi-game/js/sceneManager.js
@@ -1,1 +1,19 @@
+export class Scenes{
+  constructor(){
+    this.stack = [];
+  }
+  push(s){ this.stack.push(s); }
+  pop(){ return this.stack.pop(); }
+  clear(){ this.stack.length = 0; }
+  top(){ return this.stack[this.stack.length - 1] || null; }
+
+  update(dt){
+    const s = this.top();
+    if (s && typeof s.update === "function") s.update(dt);
+  }
+  draw(ctx){
+    const s = this.top();
+    if (s && typeof s.draw === "function") s.draw(ctx);
+  }
+}
 
