7c42b10d4a10973d521dc1125d417174ae07b21d
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:23:11 -0800

    Update hud.js

--- clawboi-game/js/hud.js
+++ clawboi-game/js/hud.js
@@ -1,15 +1,1 @@
-export function drawHUD(ctx,p){
 
-ctx.fillStyle="rgba(0,0,0,.5)";
-ctx.fillRect(8,8,180,60);
-
-ctx.fillStyle="#ff4a7a";
-ctx.fillRect(16,16,150*(p.hp/p.hpMax),8);
-
-ctx.fillStyle="#b388ff";
-ctx.fillRect(16,30,150*(p.xp/p.next),6);
-
-ctx.fillStyle="#fff";
-ctx.fillText("LV "+p.level,16,52);
-
-}
