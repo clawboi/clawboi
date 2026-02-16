6991a46dc638013936b800b2ddfab946947714fe
clawboi <ethandawsonsound@gmail.com>
2026-02-16 04:37:59 -0800

    Update renderer.js

--- clawboi-game/js/renderer.js
+++ clawboi-game/js/renderer.js
@@ -1,13 +1,13 @@
 export class Renderer{
-
-constructor(c){
-this.c=c;
-this.ctx=c.getContext("2d");
-}
-
-clear(col){
-this.ctx.fillStyle=col;
-this.ctx.fillRect(0,0,this.c.width,this.c.height);
-}
-
+  constructor(canvas){
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d", { alpha:true });
+    this.ctx.imageSmoothingEnabled = false;
+  }
+  clear(color){
+    const c = this.ctx;
+    c.setTransform(1,0,0,1,0,0);
+    c.fillStyle = color;
+    c.fillRect(0,0,this.canvas.width,this.canvas.height);
+  }
 }
