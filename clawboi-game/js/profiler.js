e86877308745781e3292adfffeec144a729fb2ee
clawboi <ethandawsonsound@gmail.com>
2026-02-16 05:38:17 -0800

    Update profiler.js

--- clawboi-game/js/profiler.js
+++ clawboi-game/js/profiler.js
@@ -1,1 +1,14 @@
+import { lerp } from "./utils.js";
+
+export class Profiler{
+  constructor(){
+    this.fps = 0;
+    this._fpsS = 0;
+  }
+  tick(dt){
+    const fps = dt > 0 ? 1/dt : 0;
+    this._fpsS = lerp(this._fpsS || fps, fps, 0.08);
+    this.fps = (this._fpsS|0);
+  }
+}
 
