75e0fc66b0ef27c93967cd0eb2ce642836521a4f
clawboi <ethandawsonsound@gmail.com>
2026-02-17 20:52:38 -0800

    Update main.js

--- NPC-City/src/main.js
+++ NPC-City/src/main.js
@@ -1,1 +1,18 @@
+import { Assets } from "./core/assets.js";
+import { Input } from "./core/input.js";
+import { Save } from "./core/save.js";
+import { Game } from "./core/game.js";
+import { UI } from "./ui/ui.js";
+import { World } from "./world/world.js";
 
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d", { alpha: false });
+
+const input = new Input(canvas);
+const save = new Save("NPC_CITY_SAVE_V1");
+const ui = new UI(document.getElementById("ui-root"));
+const assets = new Assets();
+const world = new World();
+const game = new Game({ canvas, ctx, input, save, ui, assets, world });
+
+game.boot();
